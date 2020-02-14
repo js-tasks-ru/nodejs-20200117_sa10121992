d4d537d758753888568d02e81b8640df603de3e9
anikolaev <sa10121992@yandex.ru>
2020-02-14 23:21:49 +1000

    solution for 2 task of 6 module

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,20 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const list = await Category.find();
+
+  ctx.body = {categories: list.map((category) => mapCategory(category))};
 };
+
+function mapCategory(category) {
+  const obj = {
+    id: category._id,
+    title: category.title,
+  };
+
+  if (category.subcategories) {
+    obj.subcategories = category.subcategories.map((subcategory) => mapCategory(subcategory));
+  }
+
+  return obj;
+}
