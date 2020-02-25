db22cd2b2eb748907183695b50f5303a12f83ea5
anikolaev <a.nikolaev@vl.ru>
2020-02-25 18:43:34 +1000

    solution for 2 task of 8 module

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,8 +1,38 @@
+const Product = require('../models/Product');
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const {product: productId, address, phone} = ctx.request.body;
+  const product = await Product.findById(productId);
+
+  // if ( ! product || ! address || ! phone) {
+  //   throw new Error({name: 'ValidationError'});
+  // }
+
+  const order = await Order.create({
+    user: ctx.user.id,
+    product: productId,
+    address,
+    phone,
+  });
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: {id: order.id, product},
+    to: ctx.user.email,
+    subject: 'Ваш заказ',
+  });
+
+  ctx.body = {
+    order: order.id,
+  };
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const orderList = await Order.find({user: ctx.user.id});
+
+  ctx.body = {
+    orders: orderList,
+  };
 };
