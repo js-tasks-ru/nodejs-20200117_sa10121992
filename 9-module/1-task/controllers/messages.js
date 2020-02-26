2b43c7b9a4dd6e9bea8ae4f60edcdb576fe1a0b1
anikolaev <a.nikolaev@vl.ru>
2020-02-26 10:08:34 +1000

    add solution for 9 module

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,8 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const messageList = await Message.find({user: ctx.user.displayName});
+
+  ctx.body = {messages: messageList.map(mapMessage)};
 };
--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,27 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
-    next();
+    const token = socket.handshake.query.token;
+    const session = await Session.findOne({token}).populate('user');
+
+    if (session) {
+      socket.user = session.user;
+
+      next();
+    } else {
+      next(new Error('anonymous sessions are not allowed'));
+    }
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      await Message.create({
+        user: socket.user.displayName,
+        text: msg,
+        date: new Date,
+        chat: socket.user.id,
+      });
+    });
   });
 
   return io;
